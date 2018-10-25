1c5d09104ff2a7366a1a5002e9dd61d1f74a6bdc
AKing <jinyingjun@faceu.com>
2018-10-25 18:22:25 +0800

    入门注释

--- 07-bootsector-disk/boot_sect_disk.asm
+++ 07-bootsector-disk/boot_sect_disk.asm
@@ -6,6 +6,25 @@ disk_load:
     ; to the stack for later use.
     push dx
 
+comment /*
+
+0x02 chs_mod chs模式就是通过设置磁头号，磁道号，扇区号，调用bios的0x13的2号功能进行读取磁盘扇区
+BIOS call "INT 0x13 Function 0x2" to read sectors from disk into memory
+INT 0x13 Function 0x2
+读扇区
+(3)、功能02H  
+功能描述：读扇区 
+入口参数：AH＝02H 
+AL＝扇区数 
+CH＝柱面 
+CL＝扇区 
+DH＝磁头 
+DL＝驱动器，00H~7FH：软盘；80H~0FFH：硬盘 
+ES:BX＝缓冲区的地址 
+出口参数：CF＝0——操作成功，AH＝00H，AL＝传输的扇区数，否则，AH＝状态代码，参见功能号01H中的说明
+
+*/
+
     mov ah, 0x02 ; ah <- int 0x13 function. 0x02 = 'read'
     mov al, dh   ; al <- number of sectors to read (0x01 .. 0x80)
     mov cl, 0x02 ; cl <- sector (0x01 .. 0x11)
@@ -17,6 +36,49 @@ disk_load:
 
     ; [es:bx] <- pointer to buffer where the data will be stored
     ; caller sets it up for us, and it is actually the standard location for int 13h
+
+comment /*
+
+INT 13H，AH=02H 读扇区说明：
+调用此功能将从磁盘上把一个或更多的扇区内容读进存贮器。因为这是一个
+低级功能，在一个操作中读取的全部扇区必须在同一条磁道上（磁头号和磁道号
+相同）。BIOS不能自动地从一条磁道末尾切换到另一条磁道开始，因此用户必须
+把跨多条磁道的读操作分为若干条单磁道读操作。
+入口参数：
+AH=02H 指明调用读扇区功能。
+AL 置要读的扇区数目，不允许使用读磁道末端以外的数值，也不允许
+使该寄存器为0。
+DL 需要进行读操作的驱动器号。
+DH 所读磁盘的磁头号。
+CH 磁道号的低8位数。
+CL 低5位放入所读起始扇区号，位7-6表示磁道号的高2位。
+ES:BX 读出数据的缓冲区地址。
+返回参数：
+如果CF=1，AX中存放出错状态。读出后的数据在ES:BX区域依次排列。
+详情请参见磁盘错误状态返回码一文。
+示例：
+C_SEG SEGMENT PUBLIC
+ASSUME CS:C_SEG,DS:C_SEG
+ORG 100H
+START: JMP READ
+BUFFER DB 512 DUP(0)
+READ: PUSH CS
+POP ES
+MOV BX, OFFSET BUFFER
+MOV AX, 0201H
+MOV CX, 0001H
+MOV DX, 0000H
+INT 13H
+;读软盘A, 0面0道1扇区
+;读出后数据在BUFFER中
+JC ERROR
+……
+ERROR: ……
+C_SEG ENDS
+END START
+
+*/
+
     int 0x13      ; BIOS interrupt
     jc disk_error ; if error (stored in the carry bit)
 
